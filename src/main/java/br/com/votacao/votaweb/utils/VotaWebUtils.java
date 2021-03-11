a88b945195c90b58d2c8272faba3c5aa94021603
Álvaro Gomes <alvarofederal@gmail.com>
2021-03-11 00:39:19 -0300

    Configurando timezone de São Paulo na aplicação

--- src/main/java/br/com/votacao/votaweb/utils/VotaWebUtils.java
+++ src/main/java/br/com/votacao/votaweb/utils/VotaWebUtils.java
@@ -7,6 +7,8 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class VotaWebUtils {
 
@@ -31,8 +33,16 @@ public class VotaWebUtils {
 	public static String format(String sourceDate, Long hours) throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		Calendar calendar = Calendar.getInstance();
+//		Calendar.getInstance(TimeZone.getTimeZone("America/Sao_Paulo"));
 		calendar.setTime(sdf.parse(sourceDate));
 		calendar.add(Calendar.MINUTE, converLongToInt(hours));
+		System.out.println ("Servidor Timezone: "+TimeZone.getDefault());
+
+		TimeZone tz = TimeZone.getTimeZone("America/Sao_Paulo");
+		TimeZone.setDefault(tz);
+		Calendar ca = GregorianCalendar.getInstance(tz);
+		
+		System.out.println ("Hora correta São Paulo" + ca.getTime());
 		return sdf.format(calendar.getTime());
 	}
 
